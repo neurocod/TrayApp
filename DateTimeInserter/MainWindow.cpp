c9d0c2d08967e783edc76317e51a885052dafdbf
neurocod <neurocod@gmail.com>
2025-04-16 11:53:50 +0300

    qglobalshortcut test call + bugs fixed

--- DateTimeInserter/MainWindow.cpp
+++ DateTimeInserter/MainWindow.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 #include "MainWindow.h"
+#include "../qglobalshortcut/src/qglobalshortcut.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
+  auto gs = new QGlobalShortcut(QKeySequence("Alt+F10"), this);
+  connect(gs, &QGlobalShortcut::activated, this, [this]() {
+    setVisible(!isVisible());
+    });
 }
